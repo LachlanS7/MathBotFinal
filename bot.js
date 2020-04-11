0badccaede1af8ede48f12e66b09346fa0f438b8
LachlanS7 <57027406+LachlanS7@users.noreply.github.com>
2020-04-11 15:13:47 +0930

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on('ready', () => {
     //Daily Questions
     time = new Date();
     var i = 0;
-
+       
     if(time.getUTCHours()==5 && time.getUTCMinutes() == 38){ //that is 12:00, midday
         if (i == 0){
             client.channels.get('698306874986070046').send(`${time.getDate()+1}/${time.getMonth()+1}, Daily Question: ${data.dailyQuestions[0].question}`); 
