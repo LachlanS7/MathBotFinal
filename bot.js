1647fe37955d5150034990492eec0674ba8b1308
mitchpk <30470175+mitchpk@users.noreply.github.com>
2020-04-11 15:45:15 +0930

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,14 +18,10 @@ client.on('ready', () => {
     setInterval(() => {
         //Daily Questions
         time = new Date();
-        var i = 0;
         
         if(time.getHours() == 12 && time.getMinutes() == 0){ //that is 12:00, midday
-            if (i == 0){
-                client.channels.get('698306874986070046').send(`${time.getDate()+1}/${time.getMonth()+1}, Daily Question: ${data.dailyQuestions[0].question}`); 
-                i++;
-            }
-        } else {i = 0;}
+            client.channels.get('698306874986070046').send(`${time.getDate()+1}/${time.getMonth()+1}, Daily Question: ${data.dailyQuestions[0].question}`); 
+        }
     }, 60000);
 });
 
